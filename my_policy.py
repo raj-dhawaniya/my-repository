77573b15277ca6d530c5553030b4daa9fef3896f
raj-dhawaniya <raj.dhawaniya23b@iiitg.ac.in>
2025-03-17 15:39:11 +0530

    Update my_policy.py

--- my_policy.py
+++ my_policy.py
@@ -1,1 +1,7 @@
-my_policy.py
+import numpy as np
+
+def policy_action(params, observation):
+    W = params[:32].reshape(8, 4)  # 8 inputs x 4 outputs
+    b = params[32:].reshape(4)     # 4 biases
+    logits = np.dot(observation, W) + b
+    return np.argmax(logits)
